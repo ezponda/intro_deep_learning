730c2dfff0f1d1a271ba27aa9ca761a45b003598
PabloRR100 <pabloruizruiz10@gmail.com>
2025-04-24 11:44:25 +0200

    Added first Pytorch content

---
+++ class/Fundamentals/torch/helpers.py
@@ -0,0 +1,45 @@
+import torch
+
+torch.manual_seed(42)
+
+
+def eval_model(
+        model: torch.nn.Module,
+        data_loader: torch.utils.data.DataLoader,
+        loss_fn: torch.nn.Module,
+        accuracy_fn
+):
+    """Returns a dictionary containing the results of model predicting on data_loader.
+
+    Args:
+        model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+        data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+        loss_fn (torch.nn.Module): The loss function of model.
+        accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+
+    Returns:
+        (dict): Results of model making predictions on data_loader.
+    """
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Make predictions with the model
+            y_pred = model(X)
+
+            # Accumulate the loss and accuracy values per batch
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(
+                y_true=y,
+                y_pred=y_pred.argmax(dim=1)
+            )  # For accuracy, need the prediction labels (logits -> pred_prob -> pred_labels)
+
+        # Scale loss and acc to find the average loss/acc per batch
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+
+    return {
+        "model_name": model.__class__.__name__,  # only works when model was created with a class
+        "model_loss": loss.item(),
+        "model_acc": acc
+    }
