b1526f1eb07e5e9e59745fb1e25eb2872249efc8
PabloRR100 <p_ruizruiz@apple.com>
2025-05-01 11:12:04 +0200

    Added Dockerfiles and Intro to PyTorch notebooks

---
+++ docs/Containers/00_simple_example/app_sample/main.py
@@ -0,0 +1,26 @@
+# Fast API Application with Hello World
+# ---
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/hello/{name}")
+async def hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
---
+++ docs/Containers/01_simple_uv_example/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 01-simple-uv-example!")
+
+
+if __name__ == "__main__":
+    main()
